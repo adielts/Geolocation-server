053afc6dac9ed500ceab9397341bb18db0dcd4d7
Adiel Tsayag <adielts054@gmail.com>
2021-09-12 11:33:27 +0300

    Implement the 'Hello' API call

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var port = 8080;
 
-app.get('/',(req,res)=>{
-res.send('Hello World');
+app.get("/hello", (req, res) => {
+  res.send();
 });
 
-app.listen(port,()=>{
-    console.log('app is listening on port: '+ port);
+app.listen(port, () => {
+  console.log("app is listening on port: " + port);
 });
